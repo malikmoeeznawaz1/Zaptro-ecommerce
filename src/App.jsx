0c3017fb86eb01d8d555ad9b6391fb9223e3fc71
Malik Moeez Nawaz <2021se30@student.uet.edu.pk>
2025-10-13 04:46:29 +0500

    working to show the location in navbar that is getting but not shown correcly because not getting the city na,e

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Products from './Pages/Products'
@@ -6,11 +6,36 @@ import About from './Pages/About'
 import Contact from './Pages/Contact'
 import Cart from './Pages/Cart'
 import Navbar from './Components/Navbar'
+import axios from 'axios'
 
 const App = () => {
+  const[location, setLocation] = useState()
+
+  const getLocation = async() => {
+    navigator.geolocation.getCurrentPosition(async pos => {
+      const {latitude, longitude} = pos.coords;
+
+      const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+      
+      try {
+        const location = await axios.get(url.trim());        
+        const exactLocation = location.data.address;
+
+        setLocation(exactLocation);
+      } catch (error) {
+        console.log(error);
+        
+      }
+    })
+  } 
+
+  useEffect(()=> {
+    getLocation();
+  },[])
+
   return (
     <BrowserRouter>
-    <Navbar />
+    <Navbar location={location} />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/products' element={<Products />}></Route>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,8 +6,7 @@ import { FaCaretDown } from 'react-icons/fa'
 import { IoCartOutline } from 'react-icons/io5'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
-const Navbar = () => {
-  const location = false;
+const Navbar = ({location}) => {
   return (
     <div className='bg-white py-3 shadow-2xl'>
       <div className="max-w-6xl mx-auto flex items-center justify-between">
@@ -15,7 +14,11 @@ const Navbar = () => {
           <Link to={'/'}><h1 className='font-bold text-3xl cursor-pointer'><span className='text-red-400 font-serif'>Z</span>aptro</h1></Link>
           <div className="flex gap-1 cursor-pointer text-gray-700 items-center">
             <MapPin className='text-red-400' />
-            <span className='font-semibold '>{location ? <div></div> : "Add Address"}</span>
+            <span className='font-semibold '>{location ? <div>
+              {
+                location.road
+              }
+            </div> : "Add Address"}</span>
             <FaCaretDown />
           </div>
         </div>
