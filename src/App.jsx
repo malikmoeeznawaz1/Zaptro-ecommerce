58274a6a1e60f5182b7dcc09841b3e9bed6bd445
Malik Moeez Nawaz <2021se30@student.uet.edu.pk>
2025-10-13 02:22:28 +0500

    Routes configured in app and now designing the navabat

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Products from './Pages/Products'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Cart from './Pages/Cart'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/products' element={<Products />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
