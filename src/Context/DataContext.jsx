3a90d60d1c52f423a4734d76956e5fa63d9e5c08
Malik Moeez Nawaz <2021se30@student.uet.edu.pk>
2025-10-14 02:51:00 +0500

    I got the API not the exact data that I want so will do later

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 
 export const DataContext = createContext(null);
@@ -5,8 +6,19 @@ export const DataContext = createContext(null);
 export const DataProvider = ({children}) => {
     const [data, setData] = useState();
 
+    // fetching prods from API
+    const fetchAllProds = async() => {
+        try {
+            const res = await axios.get()
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
     const obj = {
-        data, setData
+        data, setData,
+        fetchAllProds
     }
 
     return <DataContext.Provider value={obj} >{children}</DataContext.Provider>
