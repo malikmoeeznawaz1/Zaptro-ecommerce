aebd194e665d8c44439ad1ff31b9bb3776b3f158
Malik Moeez Nawaz <2021se30@student.uet.edu.pk>
2025-10-16 06:05:14 +0500

    middle banner ready a little updated

--- src/Components/MidBanner.jsx
+++ src/Components/MidBanner.jsx
@@ -8,7 +8,7 @@ const MidBanner = () => {
         <div className='absolute inset-0 bg-black/50 rounded-2xl flex justify-center items-center'>
             <div className='flex flex-col justify-center items-center'>
                 <p className=' text-3xl text-white font-bold items-center'>Next-Gen Electronics at Your Fingertips</p>
-                <p className='text-xl text-white items-center jus'>Discover the latest tech innovations with unbeatable prices and free shipping on all orders.</p>
+                <p className='text-xl text-white items-center jus'>Discover the latest tech innovations with unbeatable prices and free shipping on all orders</p>
                 <button className='text-white text-xl font-semibold bg-red-500 hover:bg-red-400 rounded-md mt-4 cursor-pointer px-4 py-2'>Shop Now</button>
             </div>
         </div>
